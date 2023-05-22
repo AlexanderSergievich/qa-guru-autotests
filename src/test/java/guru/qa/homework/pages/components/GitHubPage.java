ff9c11f70ff4ac4693a0169097f259ec912067b7
Alexander Sergievich <stackinthecorner@gmail.com>
2023-05-22 22:51:50 +0400

    allure reports developing

--- src/test/java/guru/qa/homework/pages/components/GitHubPage.java
+++ src/test/java/guru/qa/homework/pages/components/GitHubPage.java
@@ -1,5 +1,38 @@
 package guru.qa.homework.pages.components;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
 public class GitHubPage {
-    Sele
+    SelenideElement issuesTab = $("#issues-tab");
+    SelenideElement searchInputElement = $(".header-search-input");
+
+    public GitHubPage openPage(){
+        open("https://github.com");
+        return this;
+    }
+    public GitHubPage searchForRepository(String repo){
+        searchInputElement.click();
+        searchInputElement.sendKeys(repo);
+        searchInputElement.submit();
+        return this;
+    }
+    public GitHubPage clickOnRepositoryLink(String repo){
+        $(linkText(repo)).click();
+        return this;
+    }
+    public GitHubPage openIssuesTab() {
+        issuesTab.click();
+        return this;
+    }
+    public GitHubPage shouldSeeIssueWithNumber(int issue) {
+        $(withText("#" + issue)).should(Condition.exist);
+        return this;
+    }
+
 }
--- src/test/java/guru/qa/homework/tests/AlurePracticeTest.java
+++ src/test/java/guru/qa/homework/tests/AlurePracticeTest.java
@@ -1,9 +1,16 @@
 package guru.qa.homework.tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.Test;
 
 public class AlurePracticeTest {
+    String repo = "eroshenkoam/allure-example";
+    int issueNumber = 81;
     @Test
-    public void GitHubPageTest
+    public void GitHubPageTest(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+
 
 }
