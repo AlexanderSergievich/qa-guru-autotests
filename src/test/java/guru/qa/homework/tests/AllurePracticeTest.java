e5b6f97b0902e0230720550397bc608a0dec908d
Alexander Sergievich <stackinthecorner@gmail.com>
2023-05-24 22:31:38 +0400

    alure report done

--- src/test/java/guru/qa/homework/tests/AllurePracticeTest.java
+++ src/test/java/guru/qa/homework/tests/AllurePracticeTest.java
@@ -1,14 +1,15 @@
 package guru.qa.homework.tests;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
-import guru.qa.homework.steps.GitHubSteps;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 public class AllurePracticeTest extends TestBase {
     String repo = "eroshenkoam/allure-example";
     int issueNumber = 81;
+    String check = "asdfasdf";
     @Test
     public void GitHubListenerPageTest(){
         SelenideLogger.addListener("allure", new AllureSelenide());
@@ -31,6 +32,20 @@ public class AllurePracticeTest extends TestBase {
     }
     @Test
     public void GitHubLambdaPageTest(){
-
+        step("Open github page", () -> {
+            gitHubPage.openPage();
+        });
+        step("Search for repository", () -> {
+            gitHubPage.searchForRepository(repo);
+        });
+        step("click on repository link", () -> {
+            gitHubPage.clickOnRepositoryLink(repo);
+        });
+        step("Click on issues tab", () -> {
+            gitHubPage.openIssuesTab();
+        });
+        step("Verify there is an issue with number", () -> {
+            gitHubPage.shouldSeeIssueWithNumber(issueNumber);
+        });
     }
 }
